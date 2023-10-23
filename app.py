54b1aabe2c3d4a67de481a42339c4c4c9f71405e
Kosivtsov Evgeniy <jekakos@gmail.com>
2023-10-23 16:17:45 +0400

    added get_track_info

--- app.py
+++ app.py
@@ -10,27 +10,58 @@ load_dotenv()
 yandex_music_token = os.getenv('YANDEX_MUSIC_TOKEN')
 client = Client(yandex_music_token).init()
 
-@app.route('/get_song_link', methods=['GET'])
-def get_song_link():
+
+@app.route('/get_track_info', methods=['GET'])
+def get_track_info():
+    trackId = request.args.get('trackId')
+
+    if not trackId:
+        return jsonify({'error': 'Bad Request', 'message': 'trackId are required'}), 400
+
+    try:
+        search_result = client.tracks(f'{trackId}')
+    except yandex_music.exceptions.YandexMusicError as e:
+        return jsonify({'error': 'Yandex Music Error', 'message': str(e)}), 500
+
+    # pprint.pprint(search_result)
+    if not search_result:
+        return jsonify({'error': 'Not Found', 'message': 'No tracks found'}), 404
+
+    track = search_result[0]
+    track_url = f"https://music.yandex.ru/track/{track['id']}"
+
+    track_info = {
+        'id': track['id'],
+        'title': track['title'],
+        'artist': track['artists'][0]['name'] if track['artists'] else None,
+        'album': track['albums'][0]['title'] if track['albums'] else None,
+        'track_url': track_url
+    }
+
+    return jsonify(track_info)
+
+
+@app.route('/get_track_link', methods=['GET'])
+def get_track_link():
     artist = request.args.get('artist')
     title = request.args.get('title')
 
     if not artist or not title:
         return jsonify({'error': 'Bad Request', 'message': 'Both artist and title are required'}), 400
-    
+
     try:
-      search_result = client.search(f'{artist} {title}')
+        search_result = client.search(f'{artist} {title}')
     except yandex_music.exceptions.YandexMusicError as e:
-      return jsonify({'error': 'Yandex Music Error', 'message': str(e)}), 500
-    
-    if not search_result.tracks.results:
+        return jsonify({'error': 'Yandex Music Error', 'message': str(e)}), 500
+
+    if not search_result or not search_result.tracks.results:
         return jsonify({'error': 'Not Found', 'message': 'No tracks found'}), 404
-    
+
     first_track = search_result.tracks.results[0]
 
-    #pprint.pprint(first_track)
+    # pprint.pprint(first_track)
     track_url = f"https://music.yandex.ru/track/{first_track['id']}"
-    
+
     track_info = {
         'id': first_track['id'],
         'title': first_track['title'],
@@ -40,6 +71,7 @@ def get_song_link():
     }
 
     return jsonify(track_info)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5050, debug=True)
